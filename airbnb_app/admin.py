d2545719968d51d1cc7b1ddc6fe50265e558f2b7
Yaswanth K N <knyaswanth2001@gmail.com>
2023-06-22 19:59:32 +0530

    created admin and changed css for houseDetails page

--- airbnb_app/admin.py
+++ airbnb_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(House)
+admin.site.register(HouseImg)
+admin.site.register(Booking)
+admin.site.register(Customer)
+admin.site.register(Review)
+admin.site.register(Owner)
+
+
